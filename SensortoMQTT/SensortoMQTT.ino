9f01f1e5bb601981b1eed615c6067e5a6fc91224
Tr0nixCS <63261743+Tr0nixCS@users.noreply.github.com>
2020-12-15 12:07:28 +0100

    Update SensortoMQTT.ino

--- SensortoMQTT/SensortoMQTT.ino
+++ SensortoMQTT/SensortoMQTT.ino
@@ -129,7 +129,7 @@ void loop()
       display_values();
       displayInfo();
       TempHumi();
-      delay(600000); //10 min delay når lys nivauet er acceptabelt
+      ESP.deepSleep(300e6); //10 min delay når lys nivauet er acceptabelt
     }
     else
     {   
