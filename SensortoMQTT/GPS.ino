d924b6fef32c7b4b5db9a1d2a24e1c7953ce153e
Tr0nixCS <63261743+Tr0nixCS@users.noreply.github.com>
2020-12-15 14:45:24 +0100

    Update GPS.ino

--- SensortoMQTT/GPS.ino
+++ SensortoMQTT/GPS.ino
@@ -1,9 +1,13 @@
 void display_values()
 {
+  int time_start = millis();
   // This sketch displays information every time a new sentence is correctly encoded.
   while (gpsSerial.available() > 0)
     if (gps.encode(gpsSerial.read()))
-      displayInfo();
+
+      // 
+      if (millis() - time_start >= 5000);
+        displayInfo();
 
   // If 5000 milliseconds pass and there are no characters coming in
   // over the software serial port, show a "No GPS detected" error
@@ -18,36 +22,39 @@ void displayInfo()
 {
   if (gps.location.isValid())
   {
+    // Converting the GPS data to string variables
     latitude = String(gps.location.lat(), 6);
     longitude = String(gps.location.lng(), 6);
+    
     coordinates = latitude + " " + longitude;
+    
+    // Publishes the GPS location to the GPS location topic
     client.publish(topic3, String(coordinates).c_str());
-    Serial.print("Latitude: ");
     Serial.println(gps.location.lat(), 6);
-    Serial.print("Longitude: ");
-    Serial.println(gps.location.lng(), 6);
   }
   else
   {
+    // Publishes nothing to the topic if the GPS cant get any signal
     client.publish(topic3, "Not Available");
   }
   
-  //Serial.print("Time: ");
   if (gps.time.isValid())
   {
     if (gps.time.hour() < 10) Serial.print(F("0"));
-    hourtime = String(gps.time.hour() + 1);
-    minutetime = String(gps.time.minute());
-    secondtime = String(gps.time.second());
-    totaltime = hourtime + ":" + minutetime + ":" + secondtime;
-    
-    client.publish(topic4, String(totaltime).c_str());
+
+      // Converting the GPS data to string variables
+      hourtime = String(gps.time.hour() + 1);
+      minutetime = String(gps.time.minute());
+      secondtime = String(gps.time.second());
+      totaltime = hourtime + ":" + minutetime + ":" + secondtime;
+
+      // Publishes GPS time to the gps time topic
+      client.publish(topic4, String(totaltime).c_str());
   }
   else
   {
+    // Prints nothing if the GPS cant get any signal
     client.publish(topic4, 0);
   }
-  //Serial.println();
-  //Serial.println();
   delay(1000);
 }
